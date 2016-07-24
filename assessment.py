cf5e1ff667f49d563b7bb536cd10cca9b60bd838
Katia Wu <katiayx@gmail.com>
2016-07-24 05:06:52 +0000

    Updated with answers for Part 2-3

--- assessment.py
+++ assessment.py
@@ -33,31 +33,82 @@ Part 1: Discussion
 
 # Parts 2 through 5:
 # Create your classes and class methods
+class Student(object):
+	"""Defines commonalities among students"""
 
-class Plants(object):
-	"""defines what make plants plant"""
+
+	def __init__(self, first_name, last_name, address):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.address = address
+
+
+class Question(object):
+	"""A dictionary to store questions and their correct answers"""
+
+
+	def __init__(self, question, correct_answer):
+		self.question = question
+		self.correct_answer = correct_answer
+		exam = {}
+		exam[question] = correct_answer ##setting up a new dictionary to 
+		# hold all question/answer key value pairs
+
+
+
+	def ask_and_evaluate(self, question): 
+		"""Part 3.2: Prompt user to answer question and then evaluate whether user_answer
+		is correct."""
+		
+
+		user_answer = raw_input(question)
+		if user_answer == correct_answer:
+			return True
+
+		
+
+class Exam(Question):
+	"""docstring for Exam"""
 	
+	def __init__(self, exam_name, question): 
+		self.exam_name = exam_name
+		self.questions = []
 
-	eat = sunlight
-
-	def __init__(self, name, color)
-		self.name = name
-		self.color = color
-
-	def what_are_you(self, species):
-		if self.species = root:
-			print "{}! You are a root plant of {} species".format(self.name, self.species)
-		else:
-			print "{}! You are a non-root plant of {} species".format(self.name, self.species)
-
-class Veggies(Plants):
-	"""veggies are part of Plants class"""
 	
-	color = green
+	def exam_question(self, question, correct_answer):
+		"""not sure how to answer Part 3.1: in order for the method to take 
+		question, correct_answer as parameters, I'm thinking class Exam must be 
+		inheriting from class Question. Otherwise there is no definition for 'question' 
+		and 'correct answer' within this class. """
 
-class Flowers(Plants):
+		exam_question = {} #creating an empty dictionary 
+		#for list of exam_questions
+		for question, correct_answer in exam.items(): #iterate over each item in exam
+		#dictionary
+			exam_question.add(question, correct_answer) #add those key value pairs to new dictionary of 
+			#of exame questions
+				return exam_question #return the full list
 
-	def what_are_you(self):
-		super(Flowers, self).what_are_you(species) 
+	def exam_administer(self, exam_question):
+		"""Part 3.4: adminster the exam"""
+
+
+		score = 0
+		for question in exam_question:
+			if ask_and_evaluate(question) is:
+				score += 1
+		#in a dictionary, not sure how to gauge when the last key/value
+		#pair has been iterated. (Part 3.4)
+			
+
+
+
+		
+
+
+		
+
+		
+		
 		
 		
